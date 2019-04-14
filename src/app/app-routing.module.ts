7d162d14b296932579619d63d54f99eaf52b2772
Jonathan Ungaro <jungaro@unm.edu>
2019-04-14 16:21:37 -0400

    2nd puch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import {EmployeeHomepageComponent} from '../app/components/employee-homepage/employee-homepage.component';
 import {EmployeeLoginComponent}from '../app/components/employee-login/employee-login.component';
 import { CoverPageComponent } from './components/cover-page/cover-page.component';
+import { ManagerHomepageComponent } from './components/manager-homepage/manager-homepage.component';
+import { ManagerLoginComponent } from './components/manager-login/manager-login.component';
+import { EmployeeRequestComponent } from './components/employee-request/employee-request.component';
+import { ManagerApprovalComponent } from './components/manager-approval/manager-approval.component';
 
 
 const routes: Routes = [
@@ -12,12 +16,28 @@ const routes: Routes = [
   },
 
 {
-  path: 'homepage',
+  path: 'ehomepage',
   component: EmployeeHomepageComponent
 }, 
 { 
-  path: 'login',
+  path: 'elogin',
   component: EmployeeLoginComponent
+},
+{
+  path: 'mhomepage',
+  component: ManagerHomepageComponent
+},
+{
+  path: 'mlogin',
+  component: ManagerLoginComponent
+},
+{
+  path: 'erequest',
+  component: EmployeeRequestComponent
+},
+{
+  path: 'mapproval',
+  component: ManagerApprovalComponent
 }
 
 ];
